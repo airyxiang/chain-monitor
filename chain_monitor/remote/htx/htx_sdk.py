a83dbba3d16be19dcd3f766da6e9444f51cb19b5
airyxiang <airyxiang@126.com>
2024-10-16 16:15:36 +0800

    fix

--- chain_monitor/remote/htx/htx_sdk.py
+++ chain_monitor/remote/htx/htx_sdk.py
@@ -1,13 +1,11 @@
-import datetime
 import hashlib
-import time
 import hmac
 import base64
 import urllib
 from urllib import parse
 
 from huobi.exception.huobi_api_exception import HuobiApiException
-from huobi.utils import UrlParamsBuilder, utc_now, create_signature
+from huobi.utils import UrlParamsBuilder, utc_now
 
 from chain_monitor.configurations.configuration import HTX_WALLET_API_KEY, HTX_WALLET_API_SECRET_KEY, HTX_API_KEY, \
     HTX_API_SECRET_KEY
@@ -36,10 +34,10 @@ class HtxSDK:
         s = base64.b64encode(dig).decode()
         builder.put_url("Signature", s)
 
-    def wallet_request(self, host, path, method):
+    def wallet_request(self, host, path, method, currency='trc20eth', wallet_type='withdraw'):
         builder = UrlParamsBuilder()
-        builder.put_url("currency", "trc20eth")
-        builder.put_url("walletType", "hot")
+        builder.put_url("currency", currency)
+        builder.put_url("walletType", wallet_type)
         self.create_wallet_sign(api_key=HTX_WALLET_API_KEY, secret_key=HTX_WALLET_API_SECRET_KEY, method=method,
                                 url=f'{host}{path}', builder=builder)
         request_url = f'{host}{path}{builder.build_url()}'
--- chain_monitor/service/test.py
+++ chain_monitor/service/test.py
@@ -2,6 +2,6 @@ from chain_monitor.remote.htx.htx_sdk import HtxSDK
 
 htx_sdk = HtxSDK()
 
-print(htx_sdk.wallet_request(host='https://wallet-tron.huobi.com', path='/openapi/balance', method='GET'))
+htx_sdk.wallet_request(host='https://wallet-tron.huobi.com', path='/openapi/balance', method='GET')
 
-# print(htx_sdk.api_request(host='https://api.huobi.pro', path='/v1/account/accounts', method='GET'))
+# htx_sdk.api_request(host='https://api.huobi.pro', path='/v1/account/accounts', method='GET')
