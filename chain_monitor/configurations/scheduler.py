662963a1419e3364d39def16c55995ad59fbec8f
airyxiang <airyxiang@126.com>
2024-10-28 14:05:43 +0800

    fix message

--- chain_monitor/configurations/scheduler.py
+++ chain_monitor/configurations/scheduler.py
@@ -6,7 +6,7 @@ from chain_monitor.service.monitor_eth_task import monitor_supplemented_eth
 logger = get_logger(__name__)
 
 
-@scheduler.scheduled_job('interval', minutes=1)
+@scheduler.scheduled_job('cron', hour='0, 4, 8, 12, 16, 20', minute=0)
 def cron_eth_monitor():
     logger.info('Process eth monitor')
     monitor_supplemented_eth()
