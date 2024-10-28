30d8995be3a92d212f3cb29aed1cfc8232d1d012
airyxiang <airyxiang@126.com>
2024-10-28 09:21:19 +0800

    fix message

--- chain_monitor/configurations/scheduler.py
+++ chain_monitor/configurations/scheduler.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from chain_monitor.configurations.initialization import scheduler
 
 from chain_monitor.configurations.logger import get_logger
@@ -6,13 +8,15 @@ from chain_monitor.service.monitor_eth_task import monitor_supplemented_eth
 logger = get_logger(__name__)
 
 
-@scheduler.scheduled_job('interval', minutes=1)
-def cron_eth_monitor():
-    logger.info('Process eth monitor')
-    monitor_supplemented_eth()
+# @scheduler.scheduled_job('interval', minutes=1)
+# def cron_eth_monitor():
+#     logger.info('Process eth monitor')
+#     monitor_supplemented_eth()
 
 
 def run():
     logger.info("Starting scheduler")
+    scheduler.add_job(monitor_supplemented_eth, 'interval', minutes=1)
     scheduler.start()
+    asyncio.get_event_loop().run_forever()
     logger.info("Scheduler started")
